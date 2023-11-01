75511720c7778aecd98e5b6ac4b92cf815d8bcd1
Mugisha Goal <degaullemugisha@gmail.com>
2023-11-01 12:26:03 -0700

    A wqfunction that measures the balance factor of a binary tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -14,7 +14,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
-	return (left_height > right_height ? left_height : right_height) + 1;
+	return ((left_height > right_height ? left_height : right_height) + 1);
 }
 /**
  * binary_tree_balance - Measures the balance factor of a binary tree
